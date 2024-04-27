e5ca1bff6e0a44804b300cdae0d6e74ec160fab6
Pratik <127705605+Prateik26172@users.noreply.github.com>
2024-04-27 21:59:09 +0545

    all file

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,7 @@ module.exports = {
         anotherblue: "#243A66",
         Newblue: "#253A67",
         bggrey: "#DBE2EC",
-        // Gradientcolor: "",
-        // background: linear-gradient(274.26deg, #0266C9 4.14%, #1F4493 100%);
+      
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
